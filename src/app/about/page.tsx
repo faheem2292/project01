fa95f66a0642bcf91511208884ff9db0a259ae1e
faheem <m.faheem.lalian@gmail.com>
2025-12-16 11:54:04 +0500

    navbar setting

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto md:mx-0" src="/pic2.jpg" alt="" width="384" height="512"/>
-  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+  <div className="pt-6 md:p-12 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="bg-gray-800 p-4 text-white">
-        <Link href="/" className="mr-4 hover:underline">Home</Link>
-        <Link href="/about" className="mr-4 hover:underline">About</Link>
-        <Link href="/contact" className="hover:underline">Contact</Link>
+    <div className="flex justify-between items-center bg-gray-800 p-4 text-white">
+        <div className="text-xl font-bold">My Website</div>
+        <div>
+          <Link href="/" className="mr-4 hover:underline">Home</Link>
+          <Link href="/about" className="mr-4 hover:underline">About</Link>
+          <Link href="/contact" className="hover:underline">Contact</Link>
+        </div>
     </div>
   )
 }
